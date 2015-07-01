6c14ecbd3f2115a8ae52ec04f838a6a478499ace
Tate Massingill <tate.massingill@eyestreet.com>
2015-07-01 11:14:36 -0400

    [#98192030] Added RecallEvent event_id and recall_number unique index.  Updated enforcement_report util to handle duplication of data from FDA Enforcement Report CSV exports.

--- app/models/recall_event.rb
+++ app/models/recall_event.rb
@@ -63,10 +63,9 @@ class RecallEvent
 
   embeds_many :locations, as: :locatable
 
-  index 'locations.coordinates': '2d'
   index classification: 1
-
-  # index({ event_id: 1,  recall_number: 1 }, { unique: true })
+  index({ event_id: 1,  recall_number: 1 }, unique: true)
+  index 'locations.coordinates': '2d'
 
   validates :classification, :code_info, :distribution_pattern, :event_id, :initiated_by, :product_description, :product_type,
             :recall_number, :recalling_firm, :status,
